c9c70ec56b344736b286a037441e9ca6cb8616ac
Kaleck Hamm <kaleckh@gmail.com>
2021-03-20 19:51:14 -0600

    Created game layout

---
+++ src/components/Game/Game.stories.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Game from ".";
+
+export default {
+  title: "Game",
+ 
+};
+
+
+
+export const DefaultGame = () =>  <Game>
+
+</Game>
+
---
+++ src/components/Game/index.js
@@ -0,0 +1,39 @@
+import { render } from "@testing-library/react";
+import React, { Component } from "react";
+import "./Game.css";
+import Card from "../Card"
+
+
+class Game extends Component{
+    constructor(props) {
+        super(props)
+        this.state = {
+            cards: [
+                {number :1},
+                {number :2}
+            ]
+        }
+    }
+    render() {
+        return(
+            <div className="gameStyle">
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+            </div>
+        )
+            
+        
+    }
+}
+
+export default Game
