5d23933d95564433edf25f19e36ffeb0545f83be
Kaleck Hamm <kaleckh@gmail.com>
2021-04-01 20:04:34 -0600

    Added random color generation to card file

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,21 +1,27 @@
 import React, { Component } from "react";
 import "./Card.css";
+const WHITE_HEX = 16777215
 
 class Card extends Component {
   constructor(props) {
     super(props);
     this.state = {
       showFlipSide: false,
+      randomColor: this.createRandomColor(),
     };
   }
+  createRandomColor() {
+    return "#" + Math.floor(Math.random() * WHITE_HEX).toString(16);
+  }
   render() {
-    var classname = `card ${this.state.showFlipSide ?"blue" :"red"}`  ;
     return (
       <div>
         <div
-          className={classname}
+          style={{
+            background: this.state.showFlipSide ? this.state.randomColor : "blue",
+          }}
+          className="card"
           onClick={() => {
-            
             this.setState({
               showFlipSide: !this.state.showFlipSide,
             });
--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,39 +1,33 @@
 import { render } from "@testing-library/react";
 import React, { Component } from "react";
 import "./Game.css";
-import Card from "../Card"
+import Card from "../Card";
+const WHITE_HEX = 16777215;
 
+class Game extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: [{ number: 1 }, { number: 2 }],
+    };
+  }
 
-class Game extends Component{
-    constructor(props) {
-        super(props)
-        this.state = {
-            cards: [
-                {number :1},
-                {number :2}
-            ]
-        }
-    }
-    render() {
-        return(
-            <div className="gameStyle">
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-            </div>
-        )
-            
-        
-    }
+  render() {
+    return (
+      <div className="gameStyle">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    );
+  }
 }
 
-export default Game
+export default Game;
